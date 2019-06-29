eb0f36a96fda074d3466af0f10731448b655d2b2
sanjeev singh <sanjeevks101@gmail.com>
2019-06-29 18:47:48 +0530

    add about text

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('client side JS file is loaded')
-
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
